c11697870c00f9210372e7aa3e64dddb8a7c6b0f
evilj0ker <rajatsri94@gmail.com>
2022-10-02 12:54:17 +0530

    Add deadline

--- src/main/java/greeting/service/GreetingServiceImpl.java
+++ src/main/java/greeting/service/GreetingServiceImpl.java
@@ -3,6 +3,7 @@ package greeting.service;
 import com.proto.greeting.GreetingRequest;
 import com.proto.greeting.GreetingResponse;
 import com.proto.greeting.GreetingServiceGrpc;
+import io.grpc.Context;
 import io.grpc.stub.StreamObserver;
 
 public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
@@ -64,5 +65,22 @@ public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImpl
                 responseObserver.onCompleted();
             }
         };
+    }
+
+    @Override
+    public void greetWithDeadline(final GreetingRequest request, final StreamObserver<GreetingResponse> responseObserver) {
+        final Context context = Context.current();
+        try {
+            for(int i=0; i<3; i++) {
+                if (context.isCancelled()) {
+                    return;
+                }
+                Thread.sleep(100);
+            }
+            responseObserver.onNext(GreetingResponse.newBuilder().setResult("Hello " + request.getFirstName()).build());
+            responseObserver.onCompleted();
+        } catch (final InterruptedException e) {
+            responseObserver.onError(e);
+        }
     }
 }
